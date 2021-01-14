c19bb211d664590c0eb1b715e05789261d9e550f
Domziel <dominix356@gmail.com>
2021-01-14 19:07:54 +0100

    powrot v2

--- app/src/main/java/maniananana/chm/MainActivity.java
+++ app/src/main/java/maniananana/chm/MainActivity.java
@@ -42,8 +42,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
         requestPermissions();
         fAuth = FirebaseAuth.getInstance();
         fStore = FirebaseFirestore.getInstance();
